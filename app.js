a6f754fb1176de2b833f87b714e9650c5947a64e
Philip Gosu <philipgosu2@gmail.com>
2021-12-30 15:12:12 -0500

    FULL PIPELINE TEST

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('I will keep working towards becoming great'));
+app.get('/', (req, res) => res.send('It is well'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
